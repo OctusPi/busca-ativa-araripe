66fb7e69325a2b504392a469172d102e776c6791
octuspi <octuspi@gmail.com>
2024-05-20 22:03:44 -0300

    feat: papare branche back-work to devs

---
+++ server/database/migrations/2024_05_21_002331_create_subjects_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('organ_id')->constrained('organs');
+            $table->string('name');
+            $table->integer('area');
+            $table->string('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
