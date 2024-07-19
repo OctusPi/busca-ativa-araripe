8cbd5fb9cdecc29b28e43e105e2b5bec0c8366ba
csmendes <cicero.samuel.mendes@gmail.com>
2024-07-19 19:20:43 -0300

    [component] MainNav

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <script setup>
+import { inject, onMounted, ref } from 'vue';
+import auth from '@/stores/auth';
+import http from '@/services/http';
+import forms from '@/services/forms';
+import notifys from '@/utils/notifys';
+
+const app  = inject('app')
+
+import MainNav from '@/components/MainNav.vue'
+
+const emit = defineEmits(['callAlert', 'callUpdate'])
 
 </script>
 
 <template>
-    <h1 class="fs-5 text-center text-primary">Home</h1>
+   <MainNav/>
 </template>
