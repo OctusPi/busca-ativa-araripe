0087a5c1b5794f69a901d4acb04af63bd6dd2795
octuspi <octuspi@gmail.com>
2024-07-23 20:27:31 -0300

    remove old stack

--- server/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Utils\Notify;
-use Illuminate\Support\Facades\Route;
-
-Route::fallback(function () {
-    return Response()->json(Notify::error('Acesso por caminho inválido'), 403);
-});
