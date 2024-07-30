a441b92eb8eafd3f12aa6e79370daee2e47f6f98
csmendes <cicero.samuel.mendes@gmail.com>
2024-07-30 13:40:13 -0300

    view professor

--- server/app/Http/Controllers/Teachers.php
+++ server/app/Http/Controllers/Teachers.php
@@ -15,35 +15,6 @@ class Teachers extends Controller
         parent::__construct(Teacher::class, Modules::M_TEACHERS);
     }
 
-    // public function save(Request $request)
-    // {
-    //     if ($request->id) {
-    //         return parent::save($request);
-    //     }
-
-    //     $name = $request->name;
-    //     $email = $request->email;
-    //     $cpf = $request->cpf;
-    //     $phone = $request->phone;
-    //     $qualification = $request->qualification;
-
-    //     $saveTeacher = $this->base_save($request, [
-    //         'name'          => $name,
-    //         'email'         => $email,
-    //         'cpf'           => $cpf,
-    //         'phone'         => $phone,
-    //         'qualification' => $qualification
-    //     ]);
-
-    //     return $saveTeacher;
-    // }
-
-    // function list(Request $request)
-    // {
-    //     $search = ['name', 'email'];
-    //     return $this->base_list($request, $search, ['name']);
-    // }
-
     public function selects(Request $request)
     {
         return response()->json(
