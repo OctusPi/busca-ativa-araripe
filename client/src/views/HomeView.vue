9c31ef21ce57256e72063b6f707ce892ee796be3
octuspi <octuspi@gmail.com>
2024-08-09 03:37:25 -0300

    feat: add box-dash to view dashboard

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -83,16 +83,93 @@ onMounted(() => {
                         <p class="m-0 p-0 form-text">Panorama de resultados.</p>
                     </div>
                 </div>
-                <div class="page-actions d-flex align-items-center">
-                    
-                </div>
             </header>
 
-            <!-- BOX REGISTER -->
-            <div class="box p-5 mb-3">
-                
-                Insigths de Dados Rápidos
-                
+            <div class="row g-3 mb-3">
+                <div class="col-sm-12 col-md-4">
+                    <div class="box box-dash p-4">
+                        <div class="title-dash d-flex align-items-center justify-content-between">
+                            <div>
+                                <h2>Alunos Matriculados 2024</h2>
+                                <p>Quantitativo de Alunos Matriculados no ano corrente</p>
+                            </div>
+                            <ion-icon name="people-outline" class="fs-2 ms-2"></ion-icon>
+                        </div>
+                        <div class="content-dash mt-2">
+                            ...
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div class="box box-dash p-4">
+                        <div class="title-dash d-flex align-items-center justify-content-between">
+                            <div>
+                                <h2>Alunos por Escola 2024</h2>
+                                <p>Quantitativo de Alunos por Escola no ano corrente</p>
+                            </div>
+                            <ion-icon name="home-outline" class="fs-2 ms-2"></ion-icon>
+                        </div>
+                        <div class="content-dash mt-2">
+                            ...
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div class="box box-dash p-4">
+                        <div class="title-dash d-flex align-items-center justify-content-between">
+                            <div>
+                                <h2>Professores</h2>
+                                <p>Quantitativo de Professores por qualificação</p>
+                            </div>
+                            <ion-icon name="school-outline" class="fs-2 ms-2"></ion-icon>
+                        </div>
+                        <div class="content-dash mt-2">
+                            ...
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div class="box box-dash p-4">
+                        <div class="title-dash d-flex align-items-center justify-content-between">
+                            <div>
+                                <h2>Panorama de Faltas 2024</h2>
+                                <p>Quantitativo de faltas mês a mês</p>
+                            </div>
+                            <ion-icon name="close-circle-outline" class="fs-2 ms-2"></ion-icon>
+                        </div>
+                        <div class="content-dash mt-2">
+                            ...
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div class="box box-dash p-4">
+                        <div class="title-dash d-flex align-items-center justify-content-between">
+                            <div>
+                                <h2>Faltas por Escola 2024</h2>
+                                <p>Quantitativo de faltas por escola no ano corrente</p>
+                            </div>
+                            <ion-icon name="apps-outline" class="fs-2 ms-2"></ion-icon>
+                        </div>
+                        <div class="content-dash mt-2">
+                            ...
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div class="box box-dash p-4">
+                        <div class="title-dash d-flex align-items-center justify-content-between">
+                            <div>
+                                <h2>Faltas por Série 2024</h2>
+                                <p>Quantitativo de faltas por série no ano corrente</p>
+                            </div>
+                            <ion-icon name="grid-outline" class="fs-2 ms-2"></ion-icon>
+                        </div>
+                        <div class="content-dash mt-2">
+                            ...
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -102,3 +179,24 @@ onMounted(() => {
     </div>
 
 </template>
+
+<style scoped>
+
+.box-dash{
+    height: 310px;
+    overflow: auto;
+}
+
+.title-dash *{
+    margin: 0;
+    padding: 0;
+}
+
+.title-dash h2{
+    color: var(--cl-base);
+}
+
+.title-dash p{
+    font-size: 0.8rem;
+}
+</style>
